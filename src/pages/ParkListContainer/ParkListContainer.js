5d83001b3c8a39ab5d446f7bd46bf185ba84576a
wadams <wwadams94@gmail.com>
2020-08-25 16:11:04 -0700

    useful things

--- src/pages/ParkListContainer/ParkListContainer.js
+++ src/pages/ParkListContainer/ParkListContainer.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-function ParkListContainer() {
-  return (
-    <div>
-      <h1>ParkList</h1>
-    </div>
-  );
+class ParkListContainer extends React.Component {
+  state = {
+    showSecret: false,
+  };
+
+  toggle = () => {
+    this.setState((prevState) => ({
+      showSecret: !prevState.showSecret,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>ParkList</h1>
+        <button onClick={this.toggle}>
+          {this.state.showSecret ? "Hide " : "Show "}
+          Secret
+        </button>
+        {this.state.showSecret && <p>This is a secret.</p>}
+      </div>
+    );
+  }
 }
 
 export default ParkListContainer;

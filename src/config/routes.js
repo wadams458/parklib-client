7108f2168e1cf45a8b8941526e4ec7dcd337373e
wadams <wwadams94@gmail.com>
2020-08-25 14:55:18 -0700

    added containers

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/parks" />
     </Switch>
   );
 };
---
+++ src/pages/ParkListContainer/ParkListContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ParkContainer() {
+  return (
+    <div>
+      <h1>ParkList</h1>
+    </div>
+  );
+}
+
+export default ParkListContainer;

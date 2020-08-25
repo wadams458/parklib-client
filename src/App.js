1c6a88d5ab83a3cedcf60aa90abfea198996f745
wadams <wwadams94@gmail.com>
2020-08-25 12:44:56 -0700

    front end set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import routes from './config/routes';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      { routes }
+    </div>
+  );
+}
+
+export default App;

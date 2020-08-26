250168e4bca82433ab2f976c86306c0fedc1d3fc
wadams <wwadams94@gmail.com>
2020-08-26 08:42:43 -0700

    render state

--- src/pages/ParkListContainer/ParkListContainer.js
+++ src/pages/ParkListContainer/ParkListContainer.js
@@ -8,11 +8,15 @@ class ParkListContainer extends React.Component {
     // Get all Parks
     fetch("http://localhost:4000/api/v1/parks/")
       .then((response) => response.json())
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log(result);
+        this.setState({ parks: result });
+      })
       .catch((err) => console.log(err));
   }
 
   render() {
+    console.log("Render ran. State = ", this.state);
     return <div>ParkList</div>;
   }
 }
